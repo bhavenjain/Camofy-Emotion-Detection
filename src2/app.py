9194aa6e91bd44307161bb69685c12336a69fb39
keshav251 <59304231+keshav251@users.noreply.github.com>
2020-06-13 01:42:52 +0530

    age and emotion

---
+++ src2/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, Response, jsonify, request
+from camera import VideoCamera
+import cv2
+
+app = Flask(__name__, static_url_path='/static')
+
+video_stream = VideoCamera()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    camera = VideoCamera()
+    count_frames = 0
+    while count_frames<15:
+        frame = camera.get_frame()
+        count_frames+=1
+        yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n'
+        yield frame
+        yield b'\r\n\r\n'
+        #print(camera.AgeList)
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(video_stream),
+            mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True,port="5000")
