732b9472d83f176816e9eb7309a2295ba2daaee0
bhavenjain <bhavenjain@users.noreply.github.com>
2020-07-04 00:32:19 +0530

    try and try2

--- opencv/image.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img = cv2.imread("crop.jpg")
-
-cv2.imshow("output", img)
-cv2.waitKey(0)
--- opencv/try.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-
-img = cv2.imread("cars.jpg")
-cv2.imshow("Mask", img)
-cv2.waitKey(0)
-
